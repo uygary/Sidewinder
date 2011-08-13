56c4b60d1f3a0b1c151b4cb830a2db0ad471cc3a
jimbobdog <jimbobdog@iagile2008>
2011-08-13 06:21:25 +0100

    added some error handling to process check logic

--- Solutions/Sidewinder/Distributor/WaitForProcessShutdown.cs
+++ Solutions/Sidewinder/Distributor/WaitForProcessShutdown.cs
@@ -19,19 +19,31 @@ namespace Sidewinder.Distributor
 
         public bool Execute(DistributorContext context)
         {
+            var attempt = 1;
             var timer = new Stopwatch();
-            
+            timer.Start();
+
             Console.WriteLine("\tWaiting for parent process to terminate...");
 
             do
-            {               
-                if (Process.GetProcesses().Where(p => 
-                    (string.Compare(p.ProcessName, "System") != 0) &&
-                    (string.Compare(p.ProcessName, "Idle") != 0) &&
-                    (string.Compare(p.MainModule.FileName, context.Config.Command.TargetProcessFilename) == 0))
-                        .FirstOrDefault() == null)
-                    return true;
-                
+            {
+                try
+                {
+                    var running = Process.GetProcesses().ToList();
+
+                    if (running.Where(p =>
+                        (string.Compare(p.ProcessName, "System") != 0) &&
+                        (string.Compare(p.ProcessName, "Idle") != 0) &&
+                        (string.Compare(p.MainModule.FileName, context.Config.Command.TargetProcessFilename) == 0))
+                            .FirstOrDefault() == null)
+                        return true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("\t** Failed to get process list ** ({0}), attempt:={1}", 
+                        ex.Message, attempt);
+                }
+                                
                 if (timer.Elapsed.Seconds > context.Config.Command.SecondsToWait)
                 {
                     // timeout waiting for launching process to terminate
@@ -39,8 +51,9 @@ namespace Sidewinder.Distributor
                     return false;
                 }
                 
-                Console.WriteLine(string.Format("\tParent process still running...trying again..."));
+                Console.WriteLine(string.Format("\tParent process still running...trying again (attempt:={0}...", attempt));
                 Thread.Sleep(1000);
+                attempt++;
             }
             while (true);
         }
