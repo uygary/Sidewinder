6bed0ad9770cbe11284e6619c7c4b9e63d75013c
jimbobdog <jimbobdog@iagile2008>
2011-08-29 06:43:17 +0100

    unused file

--- Automation/CopySourceToReleaseAndZip.cmd
+++
@@ -1,5 +0,0 @@
-pushd ..\
-msbuild.exe Automation\msbuild\ReleaseSource.proj /p:pProjectFolder=%cd%;pSolutionFile=Sidewinder.sln;pReleaseNumber=%1 /t:ExecuteCopyAndZip
-ren Releases\v%1\Source_v%1.zip Sidewinder_Source_v%1.zip
-popd
-
