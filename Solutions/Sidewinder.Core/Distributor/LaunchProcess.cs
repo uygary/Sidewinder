f8dfb7488bdd6a534111d330ecf48748268e9722
Jacob Eisenberg <11407547+jeitrifork@users.noreply.github.com>
2014-01-15 10:52:26 +0100

    Added option to relaunch the original process or any other process after completed update. Added option to pull Sidewinder from a custom feed instead of the official NuGet feed.

---
+++ Solutions/Sidewinder.Core/Distributor/LaunchProcess.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using Fluent.IO;
+using Sidewinder.Core.Interfaces;
+using Sidewinder.Core.Interfaces.Entities;
+
+namespace Sidewinder.Core.Distributor
+{
+    /// <summary>
+    /// This will lanuch the process that is configured to start up after update completes.
+    /// </summary>
+    public class LaunchProcess : IPipelineStep<DistributorContext>
+    {
+        public void EntryConditions(DistributorContext context)
+        {
+            
+        }
+
+        public bool Execute(DistributorContext context)
+        {
+            if (string.IsNullOrWhiteSpace(context.Config.Command.LaunchProcess))
+                return true;
+
+            using (Process.Start(new ProcessStartInfo
+            {
+                FileName = context.Config.Command.LaunchProcess,
+                Arguments = context.Config.Command.LaunchProcessArguments,
+                UseShellExecute = true
+            })) { }
+
+            return true;
+        }
+
+        public void ExitConditions(DistributorContext context)
+        {
+        }
+    }
+}
